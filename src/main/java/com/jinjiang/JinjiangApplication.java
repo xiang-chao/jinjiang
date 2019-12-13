c39eef928f25fabced6f08af5755e88ba628e693
Administrator <1234>
2019-12-13 15:16:19 +0800

    锦江项目初始化

---
+++ src/main/java/com/jinjiang/JinjiangApplication.java
@@ -0,0 +1,13 @@
+package com.jinjiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinjiangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinjiangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinjiang/JinjiangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinjiang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinjiangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
