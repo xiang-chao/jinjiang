b35d6e38d3573eff20e4ea4ee6c43d717a58d02f
mac <1234>
2019-12-19 18:14:26 +0800

    添加controller的描述信息

--- src/main/java/com/jinjiang/controller/HttpController.java
+++ src/main/java/com/jinjiang/controller/HttpController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @description:
+ * @description: 调用锦江接口那礼包（金卡）
  * @author: xiangchao
  * @date: 2019/12/19 5:53 下午
  */
